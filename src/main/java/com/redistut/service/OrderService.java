3bc1aa3e7b6089aefeb1bfe8efdcd55d37284cbd
Your Name <you@example.com>
2023-05-24 23:11:48 +0800

    test

---
+++ src/main/java/com/redistut/service/OrderService.java
@@ -0,0 +1,12 @@
+package com.redistut.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+
+    public String getOrderInfo() {
+        return "order id:111";
+    }
+}
+
